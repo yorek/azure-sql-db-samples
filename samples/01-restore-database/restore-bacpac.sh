082b7d23b657916d840aa63adc767685e46b93f8
Davide Mauri <damauri@microsoft.com>
2020-04-05 20:18:43 -0700

    improved shell script

--- samples/01-restore-database/restore-bacpac.sh
+++ samples/01-restore-database/restore-bacpac.sh
@@ -7,25 +7,38 @@ set -euo pipefail
 # https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
 # and upload it to an Azure Storage Account of your choice
 
-# Update the following variables to set the correct Azure SQL tier and the sample you want to import.
-# WideWorldImporters-Full requires Premium or BusinessCritical, while
-# WideWorldImporters-Standard requires Standard or GeneralPurpose
-declare storageResourceGroup="BC_Gen5_2"
+# Update the following variable
+# to download the Full or Standart database sample
 declare bacpacFile="WideWorldImporters-Full.bacpac"
+
+# Set the location to be in the same Region where your Azure SQL is
+declare storageLocation="WestUS2"
+
+echo "Downloading $bacpacFile..."
+rm $bacpacFile -f
+curl https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/$bacpacFile -o ./$bacpacFile
+
+echo "Creating temporary Resource Group..."
+declare storageGroup=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 12 | head -n 1)
+az group create -n $storageGroup -l $storageLocation
+
+echo "Creating temporary Storage Account..."
+declare storageAccount=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 12 | head -n 1)
+az storage account create -g $storageGroup -n $storageAccount --sku Standard_LRS
 
 # Specify your credentials
 declare sqlLogin=""
 declare sqlPassword=""
 
-# Update the following three variables
-# to match your Azure enviroment and file position
+# Update the following variables to set the correct Azure SQL tier and the sample you want to import.
+# WideWorldImporters-Full requires Premium or BusinessCritical, while
+# WideWorldImporters-Standard requires Standard or GeneralPurpose
 declare sqlResourceGroup=""
 declare sqlServer=""
 declare sqlDatabase=""
-declare sqlServiceObjective=""
-declare storageAccount=""
+declare sqlServiceObjective="BC_Gen5_2"
 
-storageKey=$(az storage account keys list -g $storageResourceGroup -n $storageAccount --query "[0].value" -o tsv)
+declare storageKey=$(az storage account keys list -g $storageGroup -n $storageAccount --query "[0].value" -o tsv)
 
 az sql db create -g $sqlResourceGroup -s $sqlServer -n $sqlDatabase  --service-objective $storageResourceGroup
 
