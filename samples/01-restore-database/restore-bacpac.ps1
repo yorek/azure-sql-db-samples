1188dd921abc904161d6f582d13d70dc50719749
Davide Mauri <damauri@microsoft.com>
2020-04-06 17:32:57 -0700

    improved powershell script

--- samples/01-restore-database/restore-bacpac.ps1
+++ samples/01-restore-database/restore-bacpac.ps1
@@ -1,28 +1,56 @@
-# Download WideWorldImporters sample database from
-# https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
-# and upload it to an Azure Storage Account of your choice
+
+
+# Update the following variable
+# to download the Full or Standart database sample
+# See https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
+# to have a list of available sample bacpacs
+$bacpacFile="WideWorldImporters-Full.bacpac"
 
 # Update the following variables to set the correct Azure SQL tier and the sample you want to import.
 # WideWorldImporters-Full requires Premium or BusinessCritical, while
 # WideWorldImporters-Standard requires Standard or GeneralPurpose
-$sqlEdition="BusinessCritical"
-$sqlSLO="BC_Gen5_2"
-$bacpacFile="WideWorldImporters-Full.bacpac"
-
-# Specify your credentials
 $sqlLogin=""
 $sqlPassword=""
-
-# Update the following three variables
-# to match your Azure environment and file position
 $sqlResourceGroup=""
 $sqlServer=""
 $sqlDatabase=""
-$storageResourceGroup=""
-$storageAccount=""
+$sqlSLO="BC_Gen5_2"
+#$sqlEdition="BusinessCritical"
 
-$storageKey=$(Get-AzStorageAccountKey -ResourceGroupName "$storageResourceGroup" -StorageAccountName "$storageAccount").Value[0]
+# Set the location to be in the same Region where your Azure SQL is
+$storageLocation="WestUS2"
 
+# Generate temporary storage account and resource group
+$storageGroup=-join ((97..122) | Get-Random -Count 12 | % {[char]$_})
+$storageAccount=-join ((97..122) | Get-Random -Count 12 | % {[char]$_})
+
+# Store local path as DownloadFile needs it
+$localFolder=Convert-Path .
+$localBacpacFile=(Join-Path $localFolder $bacpacFile)
+
+Write-Output "Downloading $bacpacFile..."
+(New-Object System.Net.WebClient).DownloadFile(
+    "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/$bacpacFile", 
+    "$localBacpacFile"
+)
+
+Write-Output "Creating temporary Resource Group '$storageGroup'..."
+$sgo = New-AzResourceGroup -Name $storageGroup -Location $storageLocation
+
+Write-Output "Creating temporary Storage Account '$storageAccount'..."
+$sao = New-AzStorageAccount -ResourceGroupName $storageGroup -Name $storageAccount -SkuName Standard_LRS -Location $storageLocation
+$ctx = $sao.Context
+
+Write-Output "Creating Container..."
+New-AzStorageContainer -Name "bacpac" -Context $ctx
+
+Write-Output  "Uploading bacpac..."
+Set-AzStorageBlobContent -File "$localBacpacFile" -Container "bacpac" -Blob $bacpacFile -Context $ctx
+
+Write-Output  "Getting Storage Access Key..."
+$storageKey = $(Get-AzStorageAccountKey -ResourceGroupName "$storageResourceGroup" -StorageAccountName "$storageAccount").Value[0]
+
+Write-Output  "Importing bacpac..."
 $importRequest = New-AzSqlDatabaseImport `
     -ResourceGroupName "$sqlResourceGroup" `
     -ServerName "$sqlServer" `
@@ -30,10 +58,11 @@ $importRequest = New-AzSqlDatabaseImport `
     -DatabaseMaxSizeBytes "$(10 * 1024 * 1024 * 1024)" `
     -StorageKeyType "StorageAccessKey" `
     -StorageKey "$storageKey" `
-    -StorageUri "https://$storageAccount.blob.core.windows.net/$bacpacFile" `
-    -Edition "$sqlEdition" `
+    -StorageUri "https://$storageAccount.blob.core.windows.net/bacpac/$bacpacFile" `
     -ServiceObjectiveName "$sqlSLO" `
     -AdministratorLogin "$sqlLogin" `
     -AdministratorLoginPassword $(ConvertTo-SecureString -String "$sqlPassword" -AsPlainText -Force)
 
 Get-AzSqlDatabaseImportExportStatus -OperationStatusLink $importRequest.OperationStatusLink
+
+#    -Edition "$sqlEdition" `
