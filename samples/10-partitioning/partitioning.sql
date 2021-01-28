70e6f07a3fbb015f6033e3cc508a0f3d10878728
Davide Mauri <damauri@microsoft.com>
2021-01-28 14:35:35 -0800

    updated partitioning sample

--- samples/10-partitioning/partitioning.sql
+++ samples/10-partitioning/partitioning.sql
@@ -1,80 +1,160 @@
-SET NOCOUNT ON
-GO
+/*
+	==========
+	SETUP DEMO
+	==========
+*/
 
-SELECT * FROM sys.[external_data_sources]
-SELECT * FROM sys.[database_scoped_credentials]
-GO
+select databasepropertyex(db_name(), 'ServiceObjective')
+go
 
-DROP TABLE IF EXISTS [test].[ORDERS];
-CREATE TABLE [test].[ORDERS]
+/*
+-- Run if needed
+create schema [test]
+go
+*/
+
+drop table if exists [test].[ORDERS];
+create table [test].[ORDERS]
 (
-	[O_ORDERKEY] [INT] NOT NULL,
-	[O_CUSTKEY] [INT] NOT NULL,
-	[O_ORDERSTATUS] [CHAR](1) NOT NULL,
-	[O_TOTALPRICE] [DECIMAL](15, 2) NOT NULL,
-	[O_ORDERDATE] [DATE] NOT NULL,
-	[O_ORDERPRIORITY] [CHAR](15) NOT NULL,
-	[O_CLERK] [CHAR](15) NOT NULL,
-	[O_SHIPPRIORITY] [INT] NOT NULL,
-	[O_COMMENT] [VARCHAR](79) NOT NULL,
-	[O_ORDERYEAR]  AS (DATEPART(YEAR,[O_ORDERDATE])) PERSISTED,
-	[O_ORDERMONTH]  AS (DATEPART(MONTH,[O_ORDERDATE]))
+	[O_ORDERKEY] [int] not null,
+	[O_CUSTKEY] [int] not null,
+	[O_ORDERSTATUS] [char](1) not null,
+	[O_TOTALPRICE] [decimal](15, 2) not null,
+	[O_ORDERDATE] [date] not null,
+	[O_ORDERPRIORITY] [char](15) not null,
+	[O_CLERK] [char](15) not null,
+	[O_SHIPPRIORITY] [int] not null,
+	[O_COMMENT] [varchar](79) not null,
+	[O_ORDERYEAR] as (datepart(year,[O_ORDERDATE])) persisted,
+	[O_ORDERMONTH] as (datepart(month,[O_ORDERDATE]))
 )
+go
 
-DROP EXTERNAL DATA SOURCE [Azure-Storage];
-DROP DATABASE SCOPED CREDENTIAL [Storage-Credentials];
-GO
+/*
+-- Run if needed
+drop external data source [Azure-Storage];
+drop database scoped credential [Azure-Storage-Credentials]
+go
+*/
 
-CREATE DATABASE SCOPED CREDENTIAL [Storage-Credentials]
-WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
-SECRET = ''; -- Remove starting question mark
-GO
+select * from sys.[external_data_sources] where [name] like 'Azure-Storage%'
+select * from sys.[database_scoped_credentials] where [name] like 'Azure-Storage%'
+go 
 
-CREATE EXTERNAL DATA SOURCE [Azure-Storage]
-WITH 
+create database scoped credential [Azure-Storage-Credentials]
+with identity = 'SHARED ACCESS SIGNATURE',
+secret = 'sv=2019-12-12&st=2021-01-28T22%3A00%3A27Z&se=2021-01-29T22%3A00%3A27Z&sr=c&sp=rl&sig=0vLycIw%2Fmu7DK%2FZDVAd7hQMaT5B7b6Kccrahma8C6kk%3D'; -- Remove starting question mark
+go
+
+create external data source [Azure-Storage]
+with 
 ( 
-	TYPE = BLOB_STORAGE,
- 	LOCATION = 'https://myaccount.blob.core.windows.net',
- 	CREDENTIAL= [Storage-Credentials]
+	type = blob_storage,
+ 	location = 'https://dmstore2.blob.core.windows.net/tpch',
+ 	credential= [Azure-Storage-Credentials]
 );
 
-BULK INSERT [test].[ORDERS]
-FROM 'tpch/1GB/orders.tbl'
-WITH
+set nocount on;
+go
+
+bulk insert [test].[ORDERS]
+from '10GB/orders.tbl'
+with
 (
-	DATA_SOURCE = 'Azure-Storage',
-	FIELDTERMINATOR = '|',
-	BATCHSIZE=10000
+	data_source = 'Azure-Storage',
+	fieldterminator = '|',
+	batchsize=10000
 )
-GO
+go
 
-SELECT DISTINCT DATEFROMPARTS(YEAR(O_ORDERDATE),1,1) FROM [test].[ORDERS] ORDER BY 1
-GO
+/*
+	========
+	RUN DEMO
+	========
+*/
 
-CREATE PARTITION FUNCTION pf_OrderDate(DATE)
-AS RANGE RIGHT FOR VALUES 
+select distinct datefromparts(year(O_ORDERDATE),1,1) from [test].[ORDERS] order by 1
+go
+
+create partition function pf_OrderDate(date)
+as range right for values 
 ('1992-01-01', '1993-01-01', '1994-01-01', '1995-01-01', '1996-01-01', '1997-01-01', '1998-01-01')
-GO
+go
 
-CREATE PARTITION SCHEME ps_OrderDate 
-AS PARTITION pf_OrderDate
-ALL TO ([Primary])
-GO
+create partition scheme ps_OrderDate 
+as partition pf_OrderDate
+all to ([Primary])
+go
 
-CREATE CLUSTERED INDEX IXC ON [test].[ORDERS] (O_ORDERKEY, O_ORDERDATE) 
-WITH DROP_EXISTING 
-ON ps_OrderDate(O_ORDERDATE) 
-GO
+create clustered index IXC on [test].[ORDERS] (O_ORDERKEY, O_ORDERDATE) 
+on ps_OrderDate(O_ORDERDATE) 
+go
 
-SELECT * FROM [test].[ORDERS] WHERE O_ORDERDATE = '1995-02-07'
-GO
+select count(*) from [test].[ORDERS] where O_ORDERDATE = '1995-02-07'
+go
 
-SELECT $partition.pf_OrderDate('1992-01-01')
-GO
+select $partition.pf_OrderDate('1992-01-01')
+go
 
-TRUNCATE TABLE [test].[ORDERS] WITH (PARTITIONS (2)) 
-GO
+/*
+	truncate a partition
+*/
+truncate table [test].[ORDERS] with (partitions (2)) 
+go
 
-SELECT * FROM [test].[ORDERS] WHERE O_ORDERDATE = '1992-01-07'
-GO
+select * from [test].[ORDERS] where O_ORDERDATE = '1992-01-07'
+go
+
+/*
+	switch partition
+*/
+drop table if exists [test].[ORDERS_1995];
+create table [test].[ORDERS_1995]
+(
+	[O_ORDERKEY] [int] not null,
+	[O_CUSTKEY] [int] not null,
+	[O_ORDERSTATUS] [char](1) not null,
+	[O_TOTALPRICE] [decimal](15, 2) not null,
+	[O_ORDERDATE] [date] not null,
+	[O_ORDERPRIORITY] [char](15) not null,
+	[O_CLERK] [char](15) not null,
+	[O_SHIPPRIORITY] [int] not null,
+	[O_COMMENT] [varchar](79) not null,
+	[O_ORDERYEAR] as (datepart(year,[O_ORDERDATE])) persisted,
+	[O_ORDERMONTH] as (datepart(month,[O_ORDERDATE]))
+)
+go
+
+create clustered index IXC on [test].[ORDERS_1995] (O_ORDERKEY, O_ORDERDATE) 
+go
+
+alter table [test].[ORDERS_1995]
+add constraint check_year_1995
+check (O_ORDERDATE >= '1995-01-01' and O_ORDERDATE < '1996-01-01')
+go
+
+select $partition.pf_OrderDate('1995-01-01')
+go
+
+-- Switch Out
+alter table test.[ORDERS]
+switch partition 5 to test.[ORDERS_1995]
+go
+
+select * from [test].[ORDERS] where O_ORDERDATE = '1995-01-07'
+go
+
+select * from [test].[ORDERS_1995] where O_ORDERDATE = '1995-01-07'
+go
+
+-- Switch In
+alter table test.[ORDERS_1995]
+switch to [test].[ORDERS] partition 5
+go
+
+select * from [test].[ORDERS] where O_ORDERDATE = '1995-01-07'
+go
+
+select * from [test].[ORDERS_1995] where O_ORDERDATE = '1995-01-07'
+go
 
