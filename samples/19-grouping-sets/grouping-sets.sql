361b12be71d4c74f1beb0a90c5a8265a80f12a24
Davide Mauri <damauri@microsoft.com>
2021-10-20 15:58:21 -0700

    added grouping sets sample

---
+++ samples/19-grouping-sets/grouping-sets.sql
@@ -0,0 +1,72 @@
+/*
+    Take a look at the source data
+*/
+select top(10) * from dbo.timesheet
+
+/*
+    Return data for a dashboard
+*/
+select 
+    project, month(reported_on) as [year], sum(hours_worked) 
+from 
+    dbo.timesheet
+where
+    reported_on between '20210101' and '20211231'
+group by
+    grouping sets
+    (
+        (project),
+        (month(reported_on))       
+    )
+
+/*
+    Return data for a matrix report
+*/
+select
+    [project],
+    month([reported_on]) as [month],
+    sum([hours_worked]) as [hours_worked]
+from
+    dbo.timesheet
+where
+    reported_on between '20210101' and '20211231'
+group by
+    grouping sets
+    (
+        ([project], month([reported_on])),
+        ([project]),
+        (month([reported_on])),
+        ()
+    )
+order by 
+    [project],
+    [month]
+
+/*
+    Return data for a matrix report in a pivoted format
+*/
+with reportData as
+(
+    select
+        [project],
+        month([reported_on]) as [month],
+        sum([hours_worked]) as [hours_worked]
+    from
+        dbo.timesheet
+    where
+        reported_on between '20210101' and '20211231'
+    group by
+        grouping sets
+        (
+            ([project], month([reported_on])),
+            ([project]),
+            (month([reported_on])),
+            ()
+        )
+)
+select 
+    * 
+from
+    reportData
+pivot
+    ( sum(hours_worked) for [month] in ([1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12]) ) as p
