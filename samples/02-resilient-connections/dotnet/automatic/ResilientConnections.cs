9b6136746d07b8d2b71537b75a184a40bb406b40
Davide Mauri <damauri@microsoft.com>
2022-04-21 21:35:32 -0700

    updated to use Polly

--- samples/02-resilient-connections/dotnet/automatic/ResilientConnections.cs
+++ samples/02-resilient-connections/dotnet/automatic/ResilientConnections.cs
@@ -5,6 +5,8 @@ using Microsoft.Data.SqlClient;
 using System.Threading;
 using System.Threading.Tasks;
 using Dapper;
+using Polly;
+using System.ComponentModel;
 
 namespace AzureSQL.DevelopmentBestPractices
 {
@@ -21,7 +23,7 @@ namespace AzureSQL.DevelopmentBestPractices
 
     class SampleOptions
     {
-        public int SampleId = 0;        
+        public int SampleId = 0;
         public int SecondsConnectionStayOpen = 0;
         public int SecondsBetweenQueries = 0;
         public int SecondsQueryWillExecute = 0;
@@ -30,32 +32,29 @@ namespace AzureSQL.DevelopmentBestPractices
 
     class ResilientConnectionSample
     {
-        // Added all error code listed here: "https://docs.microsoft.com/en-us/azure/sql-database/sql-database-develop-error-messages"
-        // Plus all code experimentally observed during a loss of network connection (eg: cable unplugged)
-        private readonly List<int> _transientErrors = new List<int>() {
-                0, 
-                53, 
-                64,
-                121, 
-                258, 
-                4060, 
-                4221,
-                4891, 
-                10051,
-                10054, 
-                10065, 
-                10060, 
-                11001, 
-                18456, 
-                40197, 
-                40501, 
-                40613, 
-                49918, 
-                49919, 
-                49920
-            };
-        private int _maxAttempts = 5;
-        private int _delay = 5; // seconds
+        // Error list created from: 
+        // - https://github.com/dotnet/efcore/blob/main/src/EFCore.SqlServer/Storage/Internal/SqlServerTransientExceptionDetector.cs
+        // - https://docs.microsoft.com/en-us/dotnet/api/microsoft.data.sqlclient.sqlconfigurableretryfactory?view=sqlclient-dotnet-standard-4.1
+        // - https://docs.microsoft.com/en-us/azure/sql-database/sql-database-develop-error-messages
+        // Manually added also
+        // 0, 18456
+        private static List<int> _transientErrors = new List<int> {
+            233, 997, 921, 669, 617, 601, 121, 64, 20, 0, 53, 258,
+            1203, 1204, 1205, 1222, 1221,
+            1807,
+            3966, 3960, 3935,
+            4060, 4221, 4891,
+            8651, 8645,
+            9515,
+            14355,
+            10929, 10928, 10060, 10054, 10053, 10936, 10929, 10928, 10922, 10051, 10065,
+            11001,
+            17197,
+            18456,
+            20041,
+            41839, 41325, 41305, 41302, 41301, 40143, 40613, 40501, 40540, 40197, 49918, 49919, 49920
+        };
+
         private string _connectionString = "";
 
         public ResilientConnectionSample(string connectionString)
@@ -70,120 +69,141 @@ namespace AzureSQL.DevelopmentBestPractices
 
             var tests = new List<SampleOptions>();
 
-            tests.Add(new SampleOptions() {
+            tests.Add(new SampleOptions()
+            {
                 SampleId = 1,
                 SecondsBetweenQueries = 2,
                 SecondsConnectionStayOpen = 0,
                 SecondsQueryWillExecute = 0,
-                Token = token                
+                Token = token
             });
 
-            tests.Add(new SampleOptions() {
+            tests.Add(new SampleOptions()
+            {
                 SampleId = 2,
                 SecondsBetweenQueries = 0,
                 SecondsConnectionStayOpen = 2,
                 SecondsQueryWillExecute = 0,
-                Token = token                
+                Token = token
             });
 
-             tests.Add(new SampleOptions() {
+            tests.Add(new SampleOptions()
+            {
                 SampleId = 3,
                 SecondsBetweenQueries = 0,
                 SecondsConnectionStayOpen = 0,
                 SecondsQueryWillExecute = 2,
-                Token = token                
+                Token = token
             });
 
             var tasks = new List<Task>();
-            foreach(var t in tests)
+            foreach (var t in tests)
             {
                 tasks.Add(Task.Run(() => ExecuteSampleQuery(t)));
             }
-            
-            Console.WriteLine("Press any key to terminate the application...");
-            Console.ReadKey(true);
-            
-            source.Cancel();
-            Console.WriteLine("Cancel requested...");            
-            Task.WaitAll(tasks.ToArray());            
 
-            Console.WriteLine("Done");            
+            Console.WriteLine("Press CTRL+C to terminate the application...");
+            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs args) => {
+                if (!source.IsCancellationRequested)
+                {
+                    args.Cancel = true;
+                    source.Cancel();
+                    Console.WriteLine("Cancel requested...trying to be nice and waiting for threads to terminate. Hit CTRL+C again to terminate immediately.");
+                } else {
+                    Console.WriteLine("Terminating all threads...");
+                }
+            };
+
+            try
+            {
+                Task.WaitAll(tasks.ToArray());
+            } catch (AggregateException ae)
+            {
+                foreach(var e in ae.InnerExceptions)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.WriteLine("Done");
         }
 
         public void ExecuteSampleQuery(SampleOptions options)
-        {            
+        {
+            var p = Policy
+                .Handle<SqlException>(CheckIfTransientError)
+                .Or<TimeoutException>()
+                .OrInner<Win32Exception>(CheckIfTransientError)
+                .WaitAndRetry(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    (exception, timeSpan, retryCount, context) =>
+                    {
+                        var details = string.Empty;
+                        if (exception is SqlException) {
+                            var sx = exception as SqlException;
+                            details = $" [SQL Error: {sx.Number}]";
+                        }
+                        Console.WriteLine($"[{options.SampleId:00}] Retry called {retryCount} times, next retry in: {timeSpan}). Reason: {exception.GetType().Name}{details}: {exception.Message}");
+                    });
+
             var waitFor = string.Empty;
-            if (options.SecondsQueryWillExecute >= 0) {
+            if (options.SecondsQueryWillExecute >= 0)
+            {
                 waitFor = $"WAITFOR DELAY '00:00:{options.SecondsQueryWillExecute:00}';";
             }
-            
+
             var query = $@"
                 BEGIN TRAN; 
-                    INSERT INTO dbo.TestResiliency DEFAULT VALUES; 
+                    INSERT INTO dbo.TestResiliency (ThreadId) VALUES ({options.SampleId}); 
                     {waitFor}
                 COMMIT TRAN; 
                 SELECT @@SPID AS ServerProcessId, DATABASEPROPERTYEX(DB_NAME(DB_ID()), 'ServiceObjective') AS ServiceLevelObjective;";
 
             var csb = new SqlConnectionStringBuilder(_connectionString);
-            Console.WriteLine($"Connecting to server: '{csb.DataSource}', database: '{csb.InitialCatalog}'");
+            Console.WriteLine($"[{options.SampleId:00}] Connecting to server: '{csb.DataSource}', database: '{csb.InitialCatalog}'");
 
             while (!options.Token.IsCancellationRequested)
             {
-                int attempts = 0;
-                int waitTime = 0;
-                csb.ConnectTimeout = waitTime;
+                p.Execute(() =>{
+                    using (var conn = new SqlConnection(_connectionString))
+                    {
+                        if (options.Token.IsCancellationRequested) return;
 
-                var  conn = new SqlConnection(_connectionString);
-
-                while (attempts < _maxAttempts)
-                {
-                    attempts += 1;
-                    
-                    try
-                    {                        
-                        if (options.SecondsConnectionStayOpen >= 0) {
-                            if (options.Token.IsCancellationRequested) return;
+                        if (options.SecondsConnectionStayOpen >= 0)
+                        {
                             conn.Open();
-                            Task.Delay(options.SecondsConnectionStayOpen * 1000).Wait();                        
+                            Task.Delay(options.SecondsConnectionStayOpen * 1000).Wait(options.Token);
                         }
-                        
-                        var ei = conn.QuerySingle<ExecutionInfo>(                            
-                            query, commandTimeout: waitTime
-                            );
+
+                        if (options.Token.IsCancellationRequested) return;
+
+                        var ei = conn.QuerySingle<ExecutionInfo>(query);
 
                         if (conn.State == ConnectionState.Open) conn.Close();
-                        
-                        if (options.SecondsBetweenQueries >= 0) {
-                            if (options.Token.IsCancellationRequested) return;
-                            Task.Delay(options.SecondsBetweenQueries * 1000).Wait();                       
-                        }                         
+
+                        if (options.SecondsBetweenQueries >= 0)
+                        {
+                            Task.Delay(options.SecondsBetweenQueries * 1000).Wait(options.Token);
+                        }
 
                         Console.WriteLine($"[{options.SampleId:00}] {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} > {ei}");
-                        attempts = int.MaxValue;
                     }
-                    catch (SqlException se)
-                    {
-                        if (_transientErrors.Contains(se.Number))
-                        {
-                            waitTime = attempts * _delay;
-
-                            Console.WriteLine($"[{options.SampleId:00}] Transient error caught. Waiting {waitTime} seconds and then trying again...");
-                            Console.WriteLine($"[{options.SampleId:00}] Error: [{se.Number}] {se.Message}");
-
-                            Task.Delay(waitTime * 1000).Wait();
-                        }
-                        else
-                        {
-                            Console.WriteLine($"[{options.SampleId:00}] Unmanaged Error: [{se.Number}] {se.Message}");                            
-                            throw;
-                        }
-                    }
-                    finally
-                    {
-                        conn?.Close();
-                    }
-                }
+                });
             }
         }
+    
+        private bool CheckIfTransientError(Exception ex)
+        {
+            if (ex is SqlException sqlException)
+            {
+                foreach (SqlError err in sqlException.Errors)
+                {
+                    if (_transientErrors.Contains(err.Number)) return true;
+                }
+
+                return false;
+            }
+
+            return ex is TimeoutException;
+        }
     }
+
 }
