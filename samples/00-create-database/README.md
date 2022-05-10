3abce361c8fd93ac295b02409c129a136ef55773
Davide Mauri <damauri@microsoft.com>
2022-05-10 11:00:43 -0700

    added create database sample

---
+++ samples/00-create-database/README.md
@@ -0,0 +1,9 @@
+# 00 - Create an Azure SQL Database
+
+Creating an Azure SQL database is very easy. Here's some resources to get you started:
+
+- [Create and connect to an Azure SQL DB in 6 easy steps](https://devblogs.microsoft.com/azure-sql/create-and-connect-to-an-azure-sql-db/)
+- [Demo: Deploy Azure SQL Database | Azure SQL for beginners (Ep. 14)](https://www.youtube.com/watch?v=wiBC4OxFX60&list=PLlrxD0HtieHi5c9-i_Dnxw9vxBY-TqaeN&index=21)
+- [Quickstart: Create an Azure SQL Database single database](https://docs.microsoft.com/en-us/azure/azure-sql/database/single-database-create-quickstart?view=azuresql&tabs=azure-portal)
+
+In this repo you can also find a simple script that uses AZ CLI that shows who you can create an Azure SQL DB right away
--- samples/00-create-database/create-database.sh
+++ samples/00-create-database/create-database.sh
@@ -10,19 +10,23 @@ declare adminPassword=AzUR3SqL_PAzzw0rd!
 declare location=centralus
 declare resourceGroup=dm-serverless-2021
 
+# Create Azure SQL logical server
 az sql server create -g $resourceGroup -n $serverName \
     -l $location \
     --admin-user $adminUser \
     --admin-password $adminPassword     
 
+# Create Azure SQL database
 az sql db create -g $resourceGroup -s $serverName \
     -n $databaseName \
     --service-objective S0
 
+# Detect your public IP Address
 declare myIp=`curl https://ifconfig.me/`
 
+# Allow your IP address through the firewall
 az sql server firewall-rule create --resource-group $resourceGroup --server $serverName \
-    --name AllowMyClientIP_1 \
+    --name AllowMyClientIP \
     --start-ip-address $myIp \
     --end-ip-address $myIp
 
